7be632e77dd8fb0a5304a5c881a20a35e8b6d42a
Ravibabu Chandrasekaran <105499785+sensei-Oogway@users.noreply.github.com>
2024-04-24 16:50:01 +1000

    apps_created

---
+++ Backend/mealsOnWheels/restaurant_management_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RestaurantManagementAppConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "restaurant_management_app"
