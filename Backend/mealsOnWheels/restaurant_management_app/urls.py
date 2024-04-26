0f151db83ba50bc47478b4ccb6ae161a5c943776
Ravibabu Chandrasekaran <105499785+sensei-Oogway@users.noreply.github.com>
2024-04-26 20:06:02 +1000

    URLS_added

--- Backend/mealsOnWheels/restaurant_management_app/urls.py
+++ Backend/mealsOnWheels/restaurant_management_app/urls.py
@@ -1,4 +1,16 @@
 from django.urls import path
 from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('fetch_all_restaurants/', views.fetch_all_restaurants, name='fetch_all_restaurants'),
+    path('fetch_menu_items_by_restaurant_id/', views.fetch_menu_items_by_restaurant_id, name='fetch_menu_items_by_restaurant_id'),
+    path('create_order/', views.create_order, name='create_order'),
+    path('fetch_orders_by_customer_email/', views.fetch_orders_by_customer_email, name='fetch_orders_by_customer_email'),
+    path('provide_feedback_to_order/', views.provide_feedback_to_order, name='provide_feedback_to_order'),
+    path('create_menu_item/', views.create_menu_item, name='create_menu_item'),
+    path('edit_or_delete_menu_item/', views.edit_or_delete_menu_item, name='edit_or_delete_menu_item'),
+    path('fetch_orders_by_restaurant_id/', views.fetch_orders_by_restaurant_id, name='fetch_orders_by_restaurant_id'),
+    path('fetch_orders_by_courier_email/', views.fetch_orders_by_courier_email, name='fetch_orders_by_courier_email'),
+    path('remove_courier_request/', views.remove_courier_request, name='remove_courier_request'),
+    path('status_update/', views.status_update, name='status_update'),
+]
