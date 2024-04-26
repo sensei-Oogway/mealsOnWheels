264073669203839722220cb769bc44a17f414226
Ravibabu Chandrasekaran <105499785+sensei-Oogway@users.noreply.github.com>
2024-04-26 18:31:53 +1000

    fetch_menu_view_created

--- Backend/mealsOnWheels/restaurant_management_app/views.py
+++ Backend/mealsOnWheels/restaurant_management_app/views.py
@@ -1,5 +1,5 @@
 from django.http import JsonResponse
-from .models import Restaurant
+from .models import Restaurant, MenuItem
 
 def fetch_all_restaurants(request):
     restaurants = Restaurant.objects.all()
@@ -7,4 +7,15 @@ def fetch_all_restaurants(request):
 
     return JsonResponse(restaurant_data, safe=False)
 
+def fetch_menu_items_by_restaurant_id(request):
+    restaurant_id = request.GET.get("restaurant_id");
+    
+    try:
+        restaurant = Restaurant.objects.get(id=restaurant_id)
+        menu_items = restaurant.menu.all()
 
+        menu_items_data = list(menu_items.values())
+
+        return JsonResponse(menu_items_data, safe=False)
+    except Restaurant.DoesNotExist:
+        return JsonResponse({'error': 'Restaurant with the provided ID does not exist'}, status=404)
