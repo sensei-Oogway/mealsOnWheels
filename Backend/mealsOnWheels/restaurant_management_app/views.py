11b45bf398bcd3b39b0023e508e8e4d398081f10
Ravibabu Chandrasekaran <105499785+sensei-Oogway@users.noreply.github.com>
2024-04-26 18:28:13 +1000

    res_fetchAll_view_created

--- Backend/mealsOnWheels/restaurant_management_app/views.py
+++ Backend/mealsOnWheels/restaurant_management_app/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Restaurant
 
-# Create your views here.
+def fetch_all_restaurants(request):
+    restaurants = Restaurant.objects.all()
+    restaurant_data = list(restaurants.values())
+
+    return JsonResponse(restaurant_data, safe=False)
+
+
