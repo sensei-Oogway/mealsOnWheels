09e1dcb36ad2f407589bda5d7fa4b87be03ae3dd
akshay-2812 <akshaysparker@gmail.com>
2024-05-23 15:03:21 +1000

    change

--- webhook_receiver.py
+++ webhook_receiver.py
@@ -11,12 +11,13 @@ WEBHOOK_SECRET = "This_1s_s0_hard"
 # Endpoint to receive webhook payloads
 @app.route('/webhook', methods=['POST'])
 def webhook():
+    python_interpreter = 'C:/Users/DrAks/PycharmProjects/pythonProject/venv/Scripts/python'
     # Process the payload
     payload = request.get_json()
     if payload:
         subprocess.run(['git', 'pull'], check=True)
-        subprocess.run(['python', 'manage.py', 'migrate'], check=True)
-        subprocess.Popen(['python', 'manage.py', 'runserver'])
+        subprocess.run([python_interpreter, 'manage.py', 'migrate'], check=True)
+        subprocess.Popen([python_interpreter, 'manage.py', 'runserver'])
         print("Received payload:", payload)
         return jsonify({"message": "Webhook received successfully"}), 200
     else:
